500bc41f570557436526d3269eeaa2f04e7efc28
francisyzy <wslgit@francisyzy.com>
2022-01-08 04:13:36 +0800

    Add NUS Mods mods thingy

--- src/commands/parse.ts
+++ src/commands/parse.ts
@@ -1,19 +1,31 @@
-import { Markup } from "telegraf";
+import bot from "../lib/bot";
+import { PrismaClient } from "@prisma/client";
 import { URL } from "url";
 
-import bot from "../lib/bot";
+const prisma = new PrismaClient();
 
 const parseURL = () => {
-  bot.hears(/nusmods.com/, (ctx) => {
-    console.log(ctx.message.text);
+  bot.hears(/nusmods.com/, async (ctx) => {
     const inputURL = new URL(ctx.message.text);
-    // inputURL.searchParams.forEach((item) => {
-    //   console.log(item.);
-    // });
-    // console.log(inputURL.searchParams.keys());
-    for (const iter of inputURL.searchParams.keys()) {
-      console.log(iter);
+    const modules: string[] = [];
+    let modulesString = "";
+    for (const modCode of inputURL.searchParams.keys()) {
+      modules.push(modCode);
+      modulesString += `${modCode}, `;
     }
+    modulesString = modulesString.slice(0, -2);
+    if (modules.length === 0) {
+      return ctx.reply(
+        "Please send your share/sync link. Like https://nusmods.com/timetable/sem-2/share?CS1231S=TUT:09,LEC:1&CS2106=LAB:07,TUT:12,LEC:1&ES2660=SEC:G18&GEC1015=LEC:3&MA1521=LEC:1,TUT:1",
+      );
+    }
+    await prisma.user.update({
+      where: { telegramId: ctx.from.id },
+      data: { modules: modules },
+    });
+    return ctx.reply(
+      `You have registered the following modules: ${modulesString}`,
+    );
   });
 };
 
