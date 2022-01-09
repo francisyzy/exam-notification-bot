14083ea8dd7726734ecc34484f727def12259952
francisyzy <wslgit@francisyzy.com>
2022-01-09 14:34:49 +0800

    Fix botCommands throw 400
    Add checks to ensure error 400 will not be thrown

--- src/utils/botCommands.ts
+++ src/utils/botCommands.ts
@@ -4,11 +4,20 @@ import { BotCommand } from "typegram";
  * @return {BotCommand[]} List of admin commands
  */
 export function getBotCommands(): BotCommand[] {
-  const BotCommand: BotCommand[] = [
+  // Update this list of commands
+  const rawBotCommands = [
     {
       command: "help",
       description: "Find out more on the bot",
     },
   ];
-  return BotCommand;
+
+  let botCommands: BotCommand[] = [];
+  rawBotCommands.forEach((botCommand) => {
+    botCommands.push({
+      command: botCommand.command.toLowerCase(),
+      description: botCommand.description.substring(0, 256),
+    });
+  });
+  return botCommands;
 }
